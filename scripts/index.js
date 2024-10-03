c9471668e3960bc4fb5a11a5d03030a12dd3e4db
karpov2057 <karpov2057@yandex.ru>
2024-10-03 19:33:51 +0500

    Написал Java

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placeList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-
+const createCard = function(element) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = "Фотография";
+  placeList.append(cardElement);
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+};
+ 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.card').remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(createCard);
+   
